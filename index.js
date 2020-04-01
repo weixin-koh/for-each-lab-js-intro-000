de450df8a74de873912a46e4fa8d96606999c7d7
Wei Xin Koh <e0201350@u.nus.edu>
2020-04-01 16:58:16 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function iterativeLog(array) {
+  array.forEach(element => {
+    console.log(`${index}: ${element}`);
+  });
+}
+
+function iterate(callback) {
+  var array = [1, 2, 3];
+  array.forEach(callback);
+
+  return array;
+}
+
+function doToArray(array, callback) {
+  array.forEach(callback);
+}
