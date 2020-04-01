75cad24c07f6a2ff8955b83163eb872565c6a315
Wei Xin Koh <e0201350@u.nus.edu>
2020-04-01 17:04:12 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function iterativeLog(array) {
+  var index = 0;
+
+  array.forEach(element => {
+    console.log(`${index}: ${element}`);
+    index++;
+  });
+}
+
+function iterate(callback) {
+  var array = [1, 2, 3];
+  array.forEach(callback);
+
+  return array;
+}
+
+function doToArray(array, callback) {
+  array.forEach(callback);
+}
